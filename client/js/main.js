1c1b81a5d7a1f5df7309005b4434e5b3b169d779
Andy <6footgeek@gmail.com>
2015-05-15 19:30:58 +0100

    started on communication route and css

--- client/js/main.js
+++ client/js/main.js
@@ -14,20 +14,5 @@ jQuery(document).ready(function($) {
          }
     });
    
-    /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e){
-        
-        //store hash
-        var target = this.hash;
-                
-        e.preventDefault();
-        
-		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
-        //Collapse mobile menu after clicking
-		if ($('.navbar-collapse').hasClass('in')){
-			$('.navbar-collapse').removeClass('in').addClass('collapse');
-		}
-		
-	});
-
+   
 });
---
+++ client/js/routes/router.js
@@ -0,0 +1,27 @@
+Router.configure({
+    // the default layout
+    layoutTemplate: 'appLayout'
+});
+
+Router.route('/', function() {
+    this.layout('appLayout');
+    this.render('home');
+    this.render('splash');
+    this.render('PostFooter', {to: 'footer'});
+});
+
+Router.route('/about', function() {
+    this.render('about');
+        this.render('PostFooter', {to: 'footer'});
+});
+
+Router.route('/contact', function() {
+    this.render('contact');
+    this.render('PostFooter', {to: 'footer'});
+});
+
+
+Router.route('/connect', function() {
+    this.render('message');
+    this.render('PostFooter', {to: 'footer'});
+});
